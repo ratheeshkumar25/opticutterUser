780d7bdc2dddea8d1ca4453eca74632f12bcedef
ratheeshkumar25 <ratheeshgopinadhkumar@gmail.com>
2024-12-25 15:02:08 +0530

    db-connection updated with standard

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -19,11 +19,11 @@ func ConnectDB(config *config.Config) *gorm.DB {
 	sslmode := config.Sslmode
 
 	// Print each configuration for debugging
-	fmt.Printf("Connecting to DB: host=%s, user=%s, password=%s, dbname=%s, port=%s, sslmode=%s\n", host, user, password, dbname, port, sslmode)
+	log.Printf("Connecting to DB: host=%s, user=%s, password=%s, dbname=%s, port=%s, sslmode=%s\n", host, user, password, dbname, port, sslmode)
 
 	// Construct the DSN correctly
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, port, sslmode)
-	fmt.Println("DSN:", dsn) // Debugging line
+	log.Println("DSN:", dsn) // Debugging line
 
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -36,34 +36,8 @@ func ConnectDB(config *config.Config) *gorm.DB {
 	)
 
 	if err != nil {
-		fmt.Printf("error while migrating %v", err.Error())
+		log.Printf("error while migrating %v", err.Error())
 		return nil
 	}
 	return DB
 }
-
-// func ConnectDB(config *config.Config) *gorm.DB {
-// 	host := config.Host
-// 	user := config.User
-// 	password := config.Password
-// 	dbname := config.Database
-// 	port := config.Port
-// 	sslmode := config.Sslmode
-
-// 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, port, sslmode)
-// 	fmt.Println("DSN:", dsn)
-// 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatal("connection to the database failed:", err)
-// 	}
-// 	err = DB.AutoMigrate(
-// 		&model.User{},
-// 		&model.Address{},
-// 	)
-
-// 	if err != nil {
-// 		fmt.Printf("error while migrating %v", err.Error())
-// 		return nil
-// 	}
-// 	return DB
-// }
